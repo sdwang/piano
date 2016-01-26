035013515f63ac478c145229314f78d589084c86
Si <siwang22@gmail.com>
2016-01-25 21:53:16 -0800

    (feat) Add data file for keys

---
+++ data/data.js
@@ -0,0 +1,74 @@
+// Defines an array of data regarding keys and their properties
+var keys = [
+  {
+    note: "C",
+    keyType: "white-key",
+    keyOrder: 0,
+    url: "http://jetcityorange.com/musical-notes/C4-261.63.mp3"
+  },
+  {
+    note: "Csharp",
+    keyType: "black-key",
+    keyOrder: 0,
+    url: "http://jetcityorange.com/musical-notes/Csharp4-277.18.mp3"
+  },
+  {
+    note: "D",
+    keyType: "white-key",
+    keyOrder: 1,
+    url: "http://jetcityorange.com/musical-notes/D4-293.66.mp3"
+  },
+  {
+    note: "Dsharp",
+    keyType: "black-key",
+    keyOrder: 1,
+    url: "http://jetcityorange.com/musical-notes/Dsharp4-311.13.mp3"
+  },
+  {
+    note: "E",
+    keyType: "white-key",
+    keyOrder: 2,
+    url: "http://jetcityorange.com/musical-notes/E4-329.63.mp3"
+  },
+  {
+    note: "F",
+    keyType: "white-key",
+    keyOrder: 3,
+    url: "http://jetcityorange.com/musical-notes/F4-349.23.mp3"
+  },
+  {
+    note: "Fsharp",
+    keyType: "black-key",
+    keyOrder: 3,
+    url: "http://jetcityorange.com/musical-notes/Fsharp4-369.99.mp3"
+  },
+  {
+    note: "G",
+    keyType: "white-key",
+    keyOrder: 4,
+    url: "http://jetcityorange.com/musical-notes/G4-392.0.mp3"
+  },
+  {
+    note: "Gsharp",
+    keyType: "black-key",
+    keyOrder: 4,
+    url: "http://jetcityorange.com/musical-notes/Gsharp4-415.3.mp3"
+  },
+  {
+    note: "A",
+    keyType: "white-key",
+    keyOrder: 5,
+    url: "http://jetcityorange.com/musical-notes/A4-440.0.mp3"
+  },
+  {
+    note: "Asharp",
+    keyType: "black-key",
+    keyOrder: 5,
+    url: "http://jetcityorange.com/musical-notes/Asharp4-466.16.mp3"
+  },
+  {
+    note: "B",
+    keyType: "white-key",
+    keyOrder: 6,
+    url: "http://jetcityorange.com/musical-notes/B4-493.88.mp3"
+  }];
